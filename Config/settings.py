64364f0b688f0a2009136a2683c1c9a449415193
askvart <tur1amant@gmail.com>
2021-04-07 23:46:26 +0300

    add jpg

--- Config/settings.py
+++ Config/settings.py
@@ -122,7 +122,7 @@ STATICFILES_FINDERS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
